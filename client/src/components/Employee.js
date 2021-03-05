81277a2f3a0a4d5fcacd914af52b6384e8e5f0e6
leehanyong <god4baby@naver.com>
2021-03-05 18:14:35 +0900

    직원추가 및 파일업로드 반영

--- client/src/components/Employee.js
+++ client/src/components/Employee.js
@@ -1,22 +1,31 @@
 import React from 'react';
+
 import '../App.css';
+import { withStyles } from '@material-ui/core/styles';
+
 import Paper from '@material-ui/core/Paper';
 import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { withStyles } from '@material-ui/core/styles';
+
+import EmployeeAdd from './EmployeeAdd';
 
 const styles = theme => ({
-    root: {
+    mainDiv: {
         width: "100%",
+        textAlign: "-webkit-center"
+    },
+    
+    root: {
+        width: "70%",
         marginTop: theme.spacing(3),
         overflowX: "auto"
     },
 
     table: {
-        minWidth: "580"
+        minWidth: "540"
     }
 });
 
@@ -43,14 +52,15 @@ function CircularProgressWithLabel(props) {
 class Employee extends React.Component {
 
     state = {
-        employees : [],
-        completed : 0
+        employees: [],
+        completed: 0,
+        lodingState: 0,
     }
 
     // 컴포넌트 생성된 이후 호출
     componentDidMount() {
         this.timer = setInterval(this.progress, 30);
-        this.callApi().then(res => this.setState({employees : res})).catch(err => console.log(err));
+        this.callApi().then(res => this.setState({ employees: res })).catch(err => console.log(err));
     };
 
     // 컴포넌트가 해제되어 제거되기 직전 호출
@@ -59,16 +69,19 @@ class Employee extends React.Component {
     };
 
     // server.js api 호출
-    callApi = async() => {
+    callApi = async () => {
         const response = await fetch('/api/employee');
         const body = await response.json();
+        if (body.length >= 0) {
+            this.setState({ lodingState: 1 });
+        }
         return body;
     };
 
     // progress value 값 업데이트 함수
     progress = () => {
-        const { completed  } = this.state;
-        this.setState({ completed : completed >= 100 ? 0 : completed + 1 });
+        const { completed } = this.state;
+        this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
     };
 
     render() {
@@ -76,53 +89,56 @@ class Employee extends React.Component {
         const { classes } = this.props;
 
         return (
-            // <div style={{ height: 400, width: '90%', backgroundColor: 'dark'}}>
-            //     {this.state.employees.length > 0 
-            //         ?   <DataGrid className={classes.dataGrid} rows={this.state.employees} columns={columns} pageSize={5} checkboxSelection/>
-            //         :
-            //         <React.Fragment>
-            //             <CircularProgressWithLabel className={classes.progress} variant="determinate" value={this.state.completed} />
-            //             <LinearProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-            //         </React.Fragment>
-            //     }
-            // </div>
-            <div style={{ height: 400, width: '50%', backgroundColor: 'dark'}}>
-                {this.state.employees.length > 0 
-                    ?
-                    <Paper className={classes.root}>
-                        <Table className={classes.table}>
-                            <TableHead>
+            <div className={classes.mainDiv}> 
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
                             <TableRow>
-                                    <TableCell>번호</TableCell>
-                                    <TableCell>이미지</TableCell>
-                                    <TableCell>이름</TableCell>
-                                    <TableCell>생년월일</TableCell>
-                                    <TableCell>성별</TableCell>
-                                    <TableCell>직급</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {this.state.employees.map(c => {
-                                    return (
+                                <TableCell>번호</TableCell>
+                                <TableCell>이미지</TableCell>
+                                <TableCell>이름</TableCell>
+                                <TableCell>생년월일</TableCell>
+                                <TableCell>성별</TableCell>
+                                <TableCell>직급</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                this.state.lodingState === 0 ? (
                                     <TableRow>
-                                        <TableCell>{c.id}</TableCell>
-                                        <TableCell><img src={c.image} alt="profile"/></TableCell>
-                                        <TableCell>{c.name}</TableCell>
-                                        <TableCell>{c.birthday}</TableCell>
-                                        <TableCell>{c.gender}</TableCell>
-                                        <TableCell>{c.rank}</TableCell>
+                                        <TableCell colSpan="6" align="center">
+                                            <p>직원정보를 조회중입니다..</p>
+                                            <CircularProgressWithLabel className={classes.progress} variant="determinate" value={this.state.completed} />
+                                            <LinearProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                                        </TableCell>
                                     </TableRow>
-                                    );
-                                })}
-                            </TableBody>
-                        </Table>
-                    </Paper>   
-                    :
-                    <div>
-                        {/* <CircularProgressWithLabel className={classes.progress} variant="determinate" value={this.state.completed} /> */}
-                        <LinearProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                    </div>
-                }
+                                )
+                                    :
+                                    this.state.employees.length > 0
+                                        ?
+                                        this.state.employees.map(c => {
+                                            return (
+                                                <TableRow key={c.id}>
+                                                    <TableCell>{c.id}</TableCell>
+                                                    <TableCell><img src={c.image} alt="profile" /></TableCell>
+                                                    <TableCell>{c.name}</TableCell>
+                                                    <TableCell>{c.birthday.substring(0, 2)}년{c.birthday.substring(2, 3) === '0' ? c.birthday.substring(3, 4) + '월' : c.birthday.substring(2, 4) + '월'}{c.birthday.substring(4, 6)}일</TableCell>
+                                                    <TableCell>{c.gender}</TableCell>
+                                                    <TableCell>{c.rank}</TableCell>
+                                                </TableRow>
+                                            );
+                                        })
+                                        :
+                                        <TableRow colSpan="6" align="center">
+                                            <TableCell>
+                                                조회된 직원이 없습니다.
+                                            </TableCell>
+                                        </TableRow>
+                            }
+                        </TableBody>
+                    </Table>
+                </Paper>
+                <EmployeeAdd />
             </div>
         )
     }
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const databaseJson = fs.readFileSync('./database.json');
 const conf = JSON.parse(databaseJson);
 const mysql = require('mysql');
 
+const multer = require('multer');   // express에 multer 모듈 적용
+const upload = multer({dest: './upload'}); // 입력파일이 upload/ 폴더내에 저장
+
 const dbConnection = mysql.createConnection({
     host : conf.host,
     user : conf.user,
@@ -21,12 +24,29 @@ const dbConnection = mysql.createConnection({
 dbConnection.connect();
 
 app.get('/api/employee', (req, res) => {
-
+    
     dbConnection.query(
         'SELECT * FROM EMPLOYEE', (err, rows, fields) => {
             res.send(rows);
         }
     )
+
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/employeeAdd', upload.single('image'), (req, res) => {
+
+    let sql = 'INSERT INTO EMPLOYEE VALUES (NULL, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let rank = req.body.rank;
+    let parmas = [image, name, birthday, gender, rank];
+    dbConnection.query(sql, parmas, (err, rows, fields) => {
+        res.send(rows);
+    });
 
 });
 
